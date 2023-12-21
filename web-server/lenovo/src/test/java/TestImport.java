0ee375ea55af6bfcff44d7cc0b58aa9be351bc2d
姚世权 <yaoshiquan@zwcad.com>
2023-12-21 09:19:54 +0800

    将properties的value转成繁体中文

--- web-server/lenovo/src/test/java/TestImport.java
+++ web-server/lenovo/src/test/java/TestImport.java
@@ -1,3 +1,11 @@
+import com.github.houbb.opencc4j.util.ZhConverterUtil;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Objects;
+
 /**
  * ...
  *
@@ -5,4 +13,37 @@
  * @date 2023/11/30
  */
 public class TestImport {
+
+    @Test
+    public void simple2Traditional() {
+        String traditional = ZhConverterUtil.toTraditional("欧阳");
+        System.out.println(traditional);
+        System.out.println(System.currentTimeMillis());
+    }
+
+    /**
+     * [\u4e00-\u9fff]+=
+     */
+    @Test
+    public void test() throws IOException {
+        File srcFile = new File("E:\\Project\\ZW\\core_install\\ZWTeamworks\\config\\td\\zh.properties");
+        File targetFile = new File("E:\\Project\\ZW\\core_install\\ZWTeamworks\\config\\td\\zh_TW.properties");
+        propValueSimple2Traditional(srcFile, targetFile);
+    }
+
+    public void propValueSimple2Traditional(File srcFile, File targetFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(srcFile.toPath()), StandardCharsets.UTF_8));
+             PrintWriter writer = new PrintWriter(targetFile, "UTF-8")) {
+            String line;
+            while (Objects.nonNull(line = reader.readLine())) {
+                String[] split = line.split("=");
+                if (split.length < 2) {
+                    writer.println(line);
+                } else {
+                    String traditional = ZhConverterUtil.toTraditional(split[1]);
+                    writer.println(split[0] + "=" + traditional);
+                }
+            }
+        }
+    }
 }
