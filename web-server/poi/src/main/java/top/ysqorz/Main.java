944c8957e53c72b4e429edc3b7578c39121977c6
姚世权 <yaoshiquan@zwcad.com>
2024-01-30 13:36:48 +0800

    只能删除单行，因为行号变了

---
+++ web-server/poi/src/main/java/top/ysqorz/Main.java
@@ -0,0 +1,13 @@
+package top.ysqorz;
+
+/**
+ * ...
+ *
+ * @author yaoshiquan
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
